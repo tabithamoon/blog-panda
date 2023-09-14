dd1182caceeeaede662d999773dac8e12e4db6fb
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-09-14 16:25:23 -0300

    Switched URL for red panda image API

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,5 @@
 export async function load({ fetch, params }) {
-    const res = await fetch('https://panda-img.tabithamoon.workers.dev/');
+    const res = await fetch('https://panda-img.api.tabby.page/');
     const item = await res.json();
 
     return { item };
