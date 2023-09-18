6f8357390f1221197e914b3c8e1b6a51e60c67ec
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-09-18 16:01:30 -0300

    Add posts page

---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,16 @@
+<title>Tabitha's Blog - Posts</title>
+
+<script>
+    import Header from "../../components/Header.svelte";
+</script>
+
+<Header />
+
+<div class="flex flex-col h-screen w-screen">
+    <div class="mt-auto mx-auto mb-8">
+        <p class="text-6xl">Under construction</p>
+    </div>
+    <div class="mx-auto mb-auto">
+        <p class="text-lg">This section of the site is still in development, stay tuned!</p>
+    </div>
+</div>
