372aa215331c75169df164057858b9f634c3ddf4
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-09-18 16:01:50 -0300

    Add header to home, fix responsive design bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,26 @@
+<title>Tabitha's Blog</title>
+
 <svelte:head>
 	<link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" >
 </svelte:head>
 
 <script>
     import github_logo from "$lib/assets/github-mark-white.svg";
+    import Header from '../components/Header.svelte';
     export let data;
 </script>
 
-<title>Tabitha's Blog</title>
+<Header />
 
 <div class="flex flex-col h-screen text-white">
     <div class="flex flex-col lg:flex-row m-auto">
-        <div class="flex flex-col m-auto text-center lg:text-left text-6xl lg:text-9xl">
+        <div class="flex flex-col m-auto text-center text-6xl lg:text-left lg:text-8xl xl:text-9xl">
             <p>Hi,</p>
             <p class="text-orange-500">I'm Tabitha</p>
             <p class="text-lg lg:ml-2">I'm a red panda on the internet.</p>
         </div>
         <div class="flex flex-col min-h-full p-10">
-            <img class="object-contain max-h-96 mt-16" src="{data.item.link}" alt="a cute red panda"/>
+            <img class="object-contain max-h-80 xl:max-h-96 mt-16" src="{data.item.link}" alt="a cute red panda"/>
             <p class="text-sm pt-4 italic">{data.item.author}</p>
             <p class="text-sm italic">{data.item.source}</p>
         </div>
