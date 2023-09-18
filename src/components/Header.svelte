a51b4d146872a3552a40936a5b92cafa5741d932
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-09-18 16:01:21 -0300

    Added header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,10 @@
+<div class="fixed top-0 w-screen h-16 bg-truedarkgray drop-shadow-xl">
+    <div class="flex text-lg h-full">
+        <div class="ml-8 lg:ml-32 my-auto">
+            <a href="/">Home</a>
+        </div>
+        <div class="ml-8 my-auto">
+            <a href="/posts">Posts</a>
+        </div>
+    </div>
+</div>
