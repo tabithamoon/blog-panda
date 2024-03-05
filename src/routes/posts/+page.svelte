424a7891a9a15ec88bc490af5317498ba1a41900
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-03-05 11:22:50 -0300

    Added link to RSS feed

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -9,9 +9,14 @@
 
 <div class="flex flex-col h-screen w-screen">
     <div class="mt-32 mx-auto text-center lg:text-left lg:mx-0 lg:ml-64">
-        <p class="text-7xl">Posts</p>
+        <div class="flex flex-col lg:-ml-2">
+            <p class="text-7xl">Posts</p>
+            <a class="ml-1.5 italic text-neutral-500" href="https://feed.tabby.page">
+                RSS
+            </a>
+        </div>
 
-        {#each data.posts as {Title, Timestamp, Path, Slug}}
+        {#each data.posts as {Title, Timestamp, Slug}}
             <div class="mt-8 mx-auto">
                 <a class="text-2xl italic" href="/posts/{Slug}">{Title}</a>
                 <p class="text-base text-gray-500">{Timestamp}</p>
